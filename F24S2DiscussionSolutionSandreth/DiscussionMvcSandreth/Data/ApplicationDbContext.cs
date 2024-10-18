e9b86626d0366fbaff97d52f4763fd88242a460e
Braelynne Sandreth <bgs00011@mix.wvu.edu>
2024-10-18 21:38:59 +0000

    Emailed about having issues with my VM and I just now got it fixed. This is everything updated up to end of 10/17 class

--- F24S2DiscussionSolutionSandreth/DiscussionMvcSandreth/Data/ApplicationDbContext.cs
+++ F24S2DiscussionSolutionSandreth/DiscussionMvcSandreth/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using DiscussionLibrarySandreth;
 
 namespace DiscussionMvcSandreth.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<AppUser> AppUser { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- F24S2DiscussionSolutionSandreth/DiscussionMvcSandreth/Program.cs
+++ F24S2DiscussionSolutionSandreth/DiscussionMvcSandreth/Program.cs
@@ -1,3 +1,4 @@
+using DiscussionLibrarySandreth;
 using DiscussionMvcSandreth.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -10,11 +11,20 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+//3 Services: Database, AppUsers, Roles
+
+var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+InitialDatabase.SeedDatabase(services);
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
