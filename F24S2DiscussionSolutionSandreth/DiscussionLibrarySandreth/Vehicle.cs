2f2be399299ac1414b15b3a173ad67d5d4161347
braelynnesandreth <braelynnesandreth@gmail.com>
2024-11-18 22:01:39 -0500

    Last Update

--- F24S2DiscussionSolutionSandreth/DiscussionLibrarySandreth/Vehicle.cs
+++ F24S2DiscussionSolutionSandreth/DiscussionLibrarySandreth/Vehicle.cs
@@ -17,11 +17,6 @@ namespace DiscussionLibrarySandreth
         [NotMapped]//do not map to database, dont try to create column
         public List<ServiceRequest> ServiceRequests { get; set; }
         = new List<ServiceRequest>();
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
-        public string Color { get; set; }
-        public int Mileage { get; set; }
 
         public Vehicle() { }
 
