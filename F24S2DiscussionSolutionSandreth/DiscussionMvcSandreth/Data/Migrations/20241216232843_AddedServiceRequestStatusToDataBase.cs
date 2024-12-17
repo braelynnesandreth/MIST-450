8bebc693bd0550646f06f0ff113cbf16e4fc1314
Braelynne Sandreth <bgs00011@mix.wvu.edu>
2024-12-16 23:14:54 -0500

    Fixed supervises and service note migration errors

---
+++ F24S2DiscussionSolutionSandreth/DiscussionMvcSandreth/Data/Migrations/20241216232843_AddedServiceRequestStatusToDataBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscussionMvcSandreth.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedServiceRequestStatusToDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ServiceRequestStatus",
+                table: "ServiceRequest",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServiceRequestStatus",
+                table: "ServiceRequest");
+        }
+    }
+}
